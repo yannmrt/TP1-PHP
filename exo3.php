83d3b9db23aa13d9948891afc0463c5e8e8caa29
Yann Martin <ymartin@la-providence.net>
2020-10-06 17:21:22 +0200

    Exo3 fix

--- exo3.php
+++ exo3.php
@@ -2,28 +2,40 @@
 
 // On défini les valeurs du tableau
 $tableau = array(
-    'nom' => 'nom1, nom2, nom3',
-    'prenom' => 'prenom1, prenom2, prenom3',
-    'mdp' => 'mdp1, mdp2, mdp3'
+    array(
+        'nom' => 'nom1',
+        'prenom' => 'prenom1',
+        'mdp' => 'mdp1' 
+    ),
+    array(
+        'nom' => 'nom2',
+        'prenom' => 'prenom2',
+        'mdp' => 'mdp2' 
+    ),
+    array(
+        'nom' => 'nom3',
+        'prenom' => 'prenom3',
+        'mdp' => 'mdp3' 
+    )
 );
 
 echo '<table border="1"><tr>';
 
 for($i=1;$i<=3;$i++) {
     echo '<tr>';
-    for($j=1;$j<=1;$j++) {
+    for($j=0;$j<=5;$j++) {
         echo '<td>';
-        echo $tableau['nom'];
+        echo $tableau['nom'][$j];
         echo '</td>';
     }
     for($j=1;$j<=1;$j++) {
         echo '<td>';
-        echo $tableau['prenom'];
+        echo $tableau['prenom'][$j];
         echo '</td>';
     }
     for($j=1;$j<=1;$j++) {
         echo '<td>';
-        echo $tableau['mdp'];
+        echo $tableau['mdp'][$j];
         echo '</td>';
     }
     echo '</tr>';
